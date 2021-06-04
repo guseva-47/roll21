7fcde88cd7ecc1dc2fa3a3538327dbdff64d4884
guseva-47 <guseva-47@yandex.ru>
2021-06-05 03:20:50 +0700

    import

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,7 @@ import contextmenu from 'vue3-contextmenu';
 import 'vue3-contextmenu/dist/vue3-contextmenu.css';
 
 createApp(App)
-<<<<<<< HEAD
   .use(router)
   .use(store)
   .use(contextmenu)
   .mount("#app");
-=======
-    .use(router)
-    .use(store)
-    .mount('#app');
->>>>>>> 457af2ccbe586972ec8a058d824b896c32e58e1e
